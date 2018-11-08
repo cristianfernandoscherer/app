0523b48d3781f8796f9c807528b2ce07a0538333
Cristian <cristinfernandoscherer@gmail.com>
2018-11-08 19:27:50 -0200

    Erro

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -2,9 +2,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 
+
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
 import { ListPage } from '../pages/list/list';
+import { CadastroFuncionarioPage } from '../pages/cadastro-funcionario/cadastro-funcionario';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
@@ -13,17 +15,21 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   declarations: [
     MyApp,
     HomePage,
+    CadastroFuncionarioPage,
     ListPage
+    
+    
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp),
+    IonicModule.forRoot(MyApp)
   ],
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
     HomePage,
-    ListPage
+    ListPage,
+    CadastroFuncionarioPage
   ],
   providers: [
     StatusBar,
--- frontend/src/pages/home/home.ts
+++ frontend/src/pages/home/home.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { CadastroFuncionarioPage } from '../cadastro-funcionario/cadastro-funcionario';
 
 @Component({
   selector: 'page-home',
@@ -9,6 +10,14 @@ export class HomePage {
 
   constructor(public navCtrl: NavController) {
 
+    this.navCtrl = navCtrl;
   }
 
+
+  cadastroFunc = function(){
+    this.navCtrl.push(CadastroFuncionarioPage);
+}
+  
+  
+  
 }
